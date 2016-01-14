eb1bdb741a3ce04114c76cc4dc32e692823f1184
Joey <joeyarnstein@gmail.com>
2016-01-13 16:38:04 -0800

    almost there

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $(function(){
+
+
+
+    for (var index =99; index >= 96; index -=1) {
+//  alert(index);
+
+  $(".bottledigit").text(index);
+
+}
+
+//index.forEach(function(sofa) {
+
+//})
+});
+    //var index = [1,2,3]
+
+  //  for (var index = 1; index < 3; index +=1);
+  //    if (index >=3){
+
+
+  //var bottles =
+
+  //for (var index = 99; index >0; index -=1)
+  //var bottledigit
+
+  //for (var index= 1; index <= 3;
+  //  index += 1 {
+
+});
